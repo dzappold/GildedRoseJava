e49b5c886daadacc08016782a2066959f73ad751
Daniel <daniel.zappold@gmx.de>
2020-09-25 10:48:47 +0200

    update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -11,7 +11,7 @@ import org.gradle.plugin.use.PluginDependencySpec
  * YOU are responsible for updating manually the dependency version.
  */
 object Versions {
-    const val org_junit_jupiter: String = "5.6.0"
+    const val org_junit_jupiter: String = "5.7.0"
 
     const val de_fayard_buildsrcversions_gradle_plugin: String = "0.7.0"
 
